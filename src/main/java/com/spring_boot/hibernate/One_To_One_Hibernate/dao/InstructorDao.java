74e55fec6ce4a22e1cc08a1cd3bdf32268e845de
Suvendu Hatua <suvenduhatua01@gmail.com>
2024-12-10 23:24:01 +0530

    Adding di-directional methods to Dao class and it's implementation class.

--- src/main/java/com/spring_boot/hibernate/One_To_One_Hibernate/dao/InstructorDao.java
+++ src/main/java/com/spring_boot/hibernate/One_To_One_Hibernate/dao/InstructorDao.java
@@ -1,10 +1,16 @@
 package com.spring_boot.hibernate.One_To_One_Hibernate.dao;
 
 import com.spring_boot.hibernate.One_To_One_Hibernate.entity.Instructor;
+import com.spring_boot.hibernate.One_To_One_Hibernate.entity.InstructorDetails;
 
 public interface InstructorDao {
     public void addInstructorWithDetails(Instructor instructor);
     public Instructor findInstructorById(int id);
 
     public  void deleteInstructorById(int id);
+
+    public void addInstructorDetailsWithInstructor(InstructorDetails details);
+
+    public  InstructorDetails findInstructorDetailsById(int id);
+    public  void deleteInstructorDetailsWithOutInstructor(int id);
 }
--- src/main/java/com/spring_boot/hibernate/One_To_One_Hibernate/dao/InstructorDaoImpl.java
+++ src/main/java/com/spring_boot/hibernate/One_To_One_Hibernate/dao/InstructorDaoImpl.java
@@ -1,6 +1,7 @@
 package com.spring_boot.hibernate.One_To_One_Hibernate.dao;
 
 import com.spring_boot.hibernate.One_To_One_Hibernate.entity.Instructor;
+import com.spring_boot.hibernate.One_To_One_Hibernate.entity.InstructorDetails;
 import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -30,5 +31,22 @@ public class InstructorDaoImpl implements InstructorDao{
     public void deleteInstructorById(int id) {
         Instructor instructor=findInstructorById(id);
         entityManager.remove(instructor);
+    }
+
+    @Override
+    @Transactional
+    public void addInstructorDetailsWithInstructor(InstructorDetails details) {
+        entityManager.persist(details);
+    }
+
+    @Override
+    public InstructorDetails findInstructorDetailsById(int id) {
+        return entityManager.find(InstructorDetails.class,id);
+    }
+
+    @Override
+    @Transactional
+    public void deleteInstructorDetailsWithOutInstructor(int id) {
+
     }
 }
