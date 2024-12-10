98e43e06484311a12133a62de1c8885df380a091
Suvendu Hatua <suvenduhatua01@gmail.com>
2024-12-10 22:34:39 +0530

    Updating Instructor and InstructorDetails class and also adding OneToTone Mapping between two classes.

---
+++ src/main/java/com/spring_boot/hibernate/One_To_One_Hibernate/entity/InstructorDetails.java
@@ -0,0 +1,54 @@
+package com.spring_boot.hibernate.One_To_One_Hibernate.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "instructor-details")
+public class InstructorDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "youtube-channel")
+    private String youTubeChannel;
+
+    @Column(name = "hobbies")
+    private String hobbies;
+
+
+    //Adding setter and Getter methods-->
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getYouTubeChannel() {
+        return youTubeChannel;
+    }
+
+    public void setYouTubeChannel(String youTubeChannel) {
+        this.youTubeChannel = youTubeChannel;
+    }
+
+    public String getHobbies() {
+        return hobbies;
+    }
+
+    public void setHobbies(String hobbies) {
+        this.hobbies = hobbies;
+    }
+
+    @Override
+    public String toString() {
+        return "InstructorDetails{" +
+                "id=" + id +
+                ", youTubeChannel='" + youTubeChannel + '\'' +
+                ", hobbies='" + hobbies + '\'' +
+                '}';
+    }
+}
